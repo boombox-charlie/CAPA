35f050bb78c7bc1baf9762ad1d1b23f40cba8aef
boombox-charlie <78428194+boombox-charlie@users.noreply.github.com>
2021-02-15 08:45:48 -0600

    Create capa.js

---
+++ capa.js
@@ -0,0 +1,77 @@
+function ready(fn) {
+  if (document.readyState !== 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function handleError(err) {
+  console.error(err);
+  const target = app || data;
+//  target.invoice = '';
+  target.status = String(err).replace(/^Error: /, '');
+  console.log(data);
+}
+
+function updateInvoice(row) {
+  try {
+    data.status = '';
+    if (row === null) {
+      throw new Error("(No data - not on row - please add or select a row)");
+    }
+    console.log("GOT...", JSON.stringify(row));
+    if (row.References) {
+      try {
+        Object.assign(row, row.References);
+      } catch (err) {
+        throw new Error('Could not understand References column. ' + err);
+      }
+    }
+
+    data.invoice = Object.assign({}, data.invoice, row);
+
+    // Make invoice information available for debugging.
+    window.invoice = row;
+  } catch (err) {
+    handleError(err);
+  }
+}
+
+ready(function() {
+  // Update the invoice anytime the document data changes.
+  grist.ready();
+  grist.onRecord(updateInvoice);
+
+  // Monitor status so we can give user advice.
+  grist.on('message', msg => {
+    // If we are told about a table but not which row to access, check the
+    // number of rows.  Currently if the table is empty, and "select by" is
+    // not set, onRecord() will never be called.
+    if (msg.tableId && !app.rowConnected) {
+      grist.docApi.fetchSelectedTable().then(table => {
+        if (table.id && table.id.length >= 1) {
+          app.haveRows = true;
+        }
+      }).catch(e => console.log(e));
+    }
+    if (msg.tableId) { app.tableConnected = true; }
+    if (msg.tableId && !msg.dataChange) { app.RowConnected = true; }
+  });
+
+  Vue.config.errorHandler = function (err, vm, info)  {
+    handleError(err);
+  };
+
+  app = new Vue({
+    el: '#app',
+    data: data
+  });
+
+  if (document.location.search.includes('demo')) {
+    updateInvoice(exampleData);
+  }
+  if (document.location.search.includes('labels')) {
+    updateInvoice({});
+  }
+});
